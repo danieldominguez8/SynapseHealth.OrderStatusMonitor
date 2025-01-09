10e5213b221e383d3460f58fab3c530db76213d3
Danny Dominguez <danny.dominguez@outlook.com>
2025-01-09 11:21:50 -0600

    alert service tests added

--- SynapseHealth.OrderStatusMonitor.Tests/Services/AlertServiceTests.cs
+++ SynapseHealth.OrderStatusMonitor.Tests/Services/AlertServiceTests.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
+using SynapseHealth.OrderStatusMonitor.ConsoleApp.Services.Implementations;
+using SynapseHealth.OrderStatusMonitor.ConsoleApp.Services.Interfaces;
 
 namespace SynapseHealth.OrderStatusMonitor.Tests.Services
 {
-    internal class AlertServiceTests
+
+    public class AlertServiceTests
     {
+        private readonly Mock<IHttpClientService> _httpClientServiceMock;
+        private readonly AlertService _alertService;
+
+        public AlertServiceTests()
+        {
+            _httpClientServiceMock = new Mock<IHttpClientService>();
+            _alertService = new AlertService(_httpClientServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task SendAlertAsync_ValidMessage_SendsAlert()
+        {
+            var message = "Test alert message";
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+            _httpClientServiceMock.Setup(x => x.PostAsync(It.IsAny<string>(), It.IsAny<HttpContent>())).ReturnsAsync(responseMessage);
+
+            await _alertService.SendAlertAsync(message);
+
+            _httpClientServiceMock.Verify(x => x.PostAsync(It.Is<string>(s => s == "https://alert-api.com/alerts"), It.IsAny<HttpContent>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task SendAlertAsync_HttpClientServiceReturnsError_ThrowsException()
+        {
+            // Arrange
+            var message = "Test alert message";
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.BadRequest);
+            _httpClientServiceMock.Setup(x => x.PostAsync(It.IsAny<string>(), It.IsAny<HttpContent>())).ReturnsAsync(responseMessage);
+
+            // Act & Assert
+            await Assert.ThrowsAsync<HttpRequestException>(() => _alertService.SendAlertAsync(message));
+        }
     }
 }
